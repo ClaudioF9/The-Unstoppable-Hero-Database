84c5972df039159d5d38bb188006d9d58dcc3bb1
ClaudioF9 <claudioferreira0284@gmail.com>
2023-02-09 20:59:56 +0000

    Added function to randomise superhero id's and generate columns dynamically with images and names on page

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+//function to pull image based off id and then randomise---------------------
+let featuredCard = $("#featured-cards");
+
+//fetches image and name information
+function getImageName() {
+
+    //generates random number for id
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 731) + 1;
+      }
+    
+    let randomID = getRandomNumber();
+    
+    console.log(randomID);
+    
+    const apiKey = "10160292594515991";
+    const newUrl = "https://superheroapi.com/api.php/" + apiKey + "/" + randomID;
+    
+fetch(newUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.name);
+    console.log(data.image.url);
+
+    let featuredCardNew = $("<div>");
+    featuredCardNew.addClass("col-3 featured");
+    featuredCard.append(featuredCardNew);
+
+    let heroName = $("<p>").text(data.name);
+    let heroImageUrl = data.image.url;
+    let heroImage = $("<img>");
+    heroImage.attr("src", heroImageUrl);
+    heroImage.addClass("featured-image");
+    featuredCardNew.append(heroName,heroImage);
+    
+  })
+  .catch(function (error) {
+    console.log(error);
+    console.log("Error occurred!");
+  });
+}
+
+for (let i = 0; i < 4; i++){
+    getImageName()
+};
